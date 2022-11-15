18c307b079d0aeeb0db2bcb055d546dbc17b7de4
Александр Каурцев <91284843+kaurcev@users.noreply.github.com>
2022-11-15 14:27:15 +0600

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
-console.clear();
-
 function loading() {
+    console.clear();
+
     $.ajax({
         url: "https://get.geojs.io/v1/ip/geo.js",
         dataType: "jsonp",
@@ -12,11 +12,12 @@ function loading() {
                 console.clear();
                 console.log('%c Ваша страна не поддерживается(' + country + ')', 'background: transparent; color: #212121; font-size: 16px;');
             } else {
+                console.clear();
+                console.log('%c Стоп-стоп-стоп! \n Тут делать нечего ', 'background: transparent; color: #ff0000; font-size: 32px; text-align: left;');
+                console.log('%c Если вам интересно, то пишите сюда https://kaurcev.tk/jobs', 'background: transparent; color: #212121; font-size: 16px;');
                 console.log('%c' + country + ' - Отлично!', 'background: transparent; color: #212121; font-size: 16px;');
             }
 
         }
     });
 }
-console.log('%c Стоп-стоп-стоп! \n Тут делать нечего ', 'background: transparent; color: #ff0000; font-size: 32px; text-align: left;');
-console.log('%c Если вам интересно, то пишите сюда https://kaurcev.tk/jobs', 'background: transparent; color: #212121; font-size: 16px;');
