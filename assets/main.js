937f590fdadab81281a0a8cba2c611b8da9822fe
Александр Каурцев <91284843+kaurcev@users.noreply.github.com>
2022-11-16 23:24:57 +0600

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,16 @@
 function loading() {
     console.clear();
-
+    if ((window.location.pathname == "/heart.html") || (window.location.pathname == "/heart")) {
+        var password = prompt("Опачки.. А тут нужен пароль");
+        if (password.toLowerCase() == document.getElementById("logo").alt) {
+            document.getElementById("loading").remove();
+        } else {
+            document.getElementsByClassName("content")[0].innerHTML = "Что-то пошлло не так";
+            document.getElementById("loading").remove();
+        }
+    } else {
+        document.getElementById("loading").remove();
+    }
     $.ajax({
         url: "https://get.geojs.io/v1/ip/geo.js",
         dataType: "jsonp",
@@ -21,6 +31,38 @@ function loading() {
         }
     });
 }
+
+var deadline = '2022-10-19';
+
+function getTimeRemaining(nashe_vremya) {
+    var t = Date.parse(new Date()) - Date.parse(nashe_vremya);
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds };
+}
+
+function initializeClock(id, nashe_vremya) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(nashe_vremya);
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        if (t.total <= 0) { clearInterval(timeinterval); }
+    }
+    updateClock();
+
+    var timeinterval = setInterval(updateClock, 1000);
+}
+initializeClock('countdown', deadline);
 
 /*
 
